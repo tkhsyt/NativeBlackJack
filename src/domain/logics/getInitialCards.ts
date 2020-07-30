b0b52eb76eaaf1c44b31080576667c1be09f7c89
YutoTakahashi <yuto.takahashi2@persol.co.jp>
2020-07-30 21:46:18 +0900

    feat: TODOコメント

--- src/domain/logics/getInitialCards.ts
+++ src/domain/logics/getInitialCards.ts
@@ -2,6 +2,7 @@ import { CARD_NUMBERS } from '../const/cardNumbers';
 import { CARD_SUITS } from '../const/cardSuits';
 import { CardType } from '../declarations/models';
 
+// TODO: リファクタしてたらどっかでおかしくなったものの、なんでか調べてない
 export const getInitialCards = () => {
   return Object.values(CARD_NUMBERS).flatMap((number) =>
     Object.values(CARD_SUITS).map((suit) => [suit, number]),
