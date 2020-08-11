69e7028b192b3792abd723dcc723308ffc3bd7a8
YutoTakahashi <yuto.takahashi2@persol.co.jp>
2020-08-11 19:29:07 +0900

    feat: judgeHandStatusにtestを追加

---
+++ src/domain/logics/judgeHandStatus/index.tests.ts
@@ -0,0 +1,16 @@
+import { judgeHandStatus } from './index';
+
+describe('渡されたハンドの合計値から状態を返すロジック', () => {
+  it('ハンドの合計値を渡したらstringにして結果を返す', () => {
+    expect(judgeHandStatus(3)).toEqual('3');
+  });
+  it('21を渡したらBLACKJACK!と返す', () => {
+    expect(judgeHandStatus(21)).toEqual('BLACKJACK!');
+  });
+  it('21以上を渡したらBUSTと返す', () => {
+    expect(judgeHandStatus(22)).toEqual('BUST');
+  });
+  it('12~16を渡したら${number} stiff...と返す', () => {
+    expect(judgeHandStatus(15)).toEqual('15 stiff...');
+  });
+});
